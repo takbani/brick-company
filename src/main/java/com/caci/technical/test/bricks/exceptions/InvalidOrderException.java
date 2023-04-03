42e00b29c98f44368267c264849088e37deb7a81
taiz.akbani <taiz.akbani@outlook.com>
2023-04-03 12:49:20 +0100

    Code tidy up

--- src/main/java/com/caci/technical/test/bricks/exceptions/InvalidOrderException.java
+++ src/main/java/com/caci/technical/test/bricks/exceptions/InvalidOrderException.java
@@ -1,6 +1,6 @@
 package com.caci.technical.test.bricks.exceptions;
 
-public class InvalidOrderException extends Throwable {
+public class InvalidOrderException extends Exception {
 
     public InvalidOrderException(String message) {
         super(message);
